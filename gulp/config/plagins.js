6763500ee47d60db1873371b9076a4a10b2ff764
Nataliia Vorobei <natalia.vorobei27@gmail.com>
2023-02-12 22:14:04 +0200

    Added: gulp

---
+++ gulp/config/plagins.js
@@ -0,0 +1,14 @@
+import replace from "gulp-replace"; // Пошук і заміна
+import plumber from "gulp-plumber"; // Обробка помилок
+import notify from "gulp-notify"; // Сповіщення (підсказка)
+import browsersync from "browser-sync"; //Локальний сервер
+import newer from "gulp-newer"; // Перевірка оновлення
+
+// Експортуємо об'єкт
+export const plugins = {
+    replace: replace,
+    plumber: plumber,
+    notify: notify,
+    browsersync: browsersync,
+    newer: newer
+}
---
+++ gulp/tasks/scss.js
@@ -0,0 +1,46 @@
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import rename from "gulp-rename";
+
+import cleanCss from "gulp-clean-css"; // Зжим CSS файлу
+// import webpcss from "gulp-webpcss"; // Вивід WEBP зображень
+import autoprefixer from "gulp-autoprefixer"; // Додавання вендорних префіксів
+import groupCssMediaQueries from "gulp-group-css-media-queries"; // Групування медіа запитів
+
+
+const sass = gulpSass(dartSass);
+
+export const scss = () => {
+    return app.gulp.src(app.path.src.scss, {sourcemaps: true})
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "SCSS",
+                message: "Error: <%= error.message %>"
+            }))
+        )
+        .pipe(app.plugins.replace(/@img\//g, '../img/'))
+        .pipe(sass({
+            outputStyle: "expanded"
+        }))
+        .pipe(groupCssMediaQueries())
+        // .pipe(webpcss(
+        //     {
+        //         webpClass: ".webp",
+        //         noWebpClass: ".no-webp"
+        //     }
+        // ))
+        .pipe(autoprefixer({
+            grid: true,
+            overrideBrowserList: ["last 3 versions"],
+            cascade: true
+        }))
+        // Розкоментувати якщо потрібний не сжатий дубль файлу стилів
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(cleanCss())
+        .pipe(rename({
+            extname: ".min.css"
+        }))       
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(app.plugins.browsersync.stream());
+
+}
